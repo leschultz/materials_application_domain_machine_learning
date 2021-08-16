7110bd553329c5d6b4c8070d31ec120f3f9bdbeb
leschultz <lsschultz@wisc.edu>
2021-08-16 15:14:12 -0500

    trying to make official package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Application Domain
+
+Research with respect to application domain with a materials science emphasis is contained within. The GitHub repo can be found in [here](https://github.com/leschultz/application_domain.git).
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# Package information
+name = 'application_domain'
+version = '0.0.1'
+description = 'Application domain of machine learning in materials science.'
+url = 'https://github.com/leschultz/application_domain.git'
+author = 'Lane E. Schultz'
+author_email='laneenriqueschultz@gmail.com'
+python_requires = '>=3.6'
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               ]
+package_dir = {'': 'src'}
+packages=setuptools.find_packages(where='src')
+
+# Passing variables to setup
+setuptools.setup(
+                 name=name,
+                 version=version,
+                 description=description,
+                 url=url,
+                 author=author,
+                 author_email=author_email,
+                 package_dir=package_dir,
+                 packages=packages,
+                 python_requires=python_requires,
+                 classifiers=classifiers,
+                 )
