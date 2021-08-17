e673026effd7231f27e5f24a35cb3211fd6ef333
leschultz <lsschultz@wisc.edu>
2021-08-17 12:46:13 -0500

    Need to figure out how to include this package in tests

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 # Package information
-name = 'materials_application_domain'
+name = 'Materials Application Domain (MAD)'
 version = '0.0.1'
 description = 'Application domain of machine learning in materials science.'
 url = 'https://github.com/leschultz/application_domain.git'
