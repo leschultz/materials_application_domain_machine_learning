15220242f02881ac143d60a400833b57e864cc82
leschultz <lsschultz@wisc.edu>
2021-08-25 14:04:45 -0500

    Updated dependencies

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ install_requires = [
                     'tqdm',
                     'pytest',
                     'openpyxl',
+                    'statsmodels',
                     ]
 long_description=open('README.md').read()
 
