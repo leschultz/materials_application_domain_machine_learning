4bd96c8d2bf0fdb147a8ddfdb2d77ab92d7becf3
leschultz <lsschultz@wisc.edu>
2021-08-26 13:32:23 -0500

    Changed lable

--- mad/plots/kde_ln_likelihood_counts.py
+++ mad/plots/kde_ln_likelihood_counts.py
@@ -8,88 +8,6 @@ import os
 from mad.functions import parallel
 
 
-def binner(i, data, actual, pred, save, points, sampling):
-
-    os.makedirs(save, exist_ok=True)
-
-    name = os.path.join(save, pred)
-    name += '_{}'.format(i)
-
-    df = data[[i, actual, pred]].copy()
-
-    if sampling == 'even':
-        df['bin'] = pd.cut(
-                           df[i],
-                           points,
-                           include_lowest=True
-                           )
-
-    elif sampling == 'equal':
-        df.sort_values(by=i, inplace=True)
-        df = np.array_split(df, points)
-        count = 0
-        for j in df:
-            j['bin'] = count
-            count += 1
-
-        df = pd.concat(df)
-
-    # Statistics
-    rmses = []
-    moderrs = []
-    bins = []
-    counts = []
-    for group, values in df.groupby('bin'):
-
-        if values.empty:
-            continue
-
-        x = values[actual].values
-        y = values[pred].values
-
-        rmse = metrics.mean_squared_error(x, y)**0.5
-        moderr = np.mean(values[i].values)
-        count = values[i].values.shape[0]
-
-        rmses.append(rmse)
-        moderrs.append(moderr)
-        bins.append(group)
-        counts.append(count)
-
-    moderrs = np.array(moderrs)
-    rmses = np.array(rmses)
-
-    if 'std' in i:
-        xlabel = r'Average $\sigma_{model}$'
-    else:
-        xlabel = 'Average {}'.format(i)
-        xlabel = xlabel.replace('_', ' ')
-
-    widths = (max(moderrs)-min(moderrs))/len(moderrs)*0.5
-    fig, ax = pl.subplots(2)
-
-    ax[0].plot(moderrs, rmses, marker='.', linestyle='none')
-    ax[1].bar(moderrs, counts, widths)
-
-    ax[0].set_ylabel(r'$RMSE$')
-
-    ax[1].set_xlabel(xlabel)
-    ax[1].set_ylabel('Counts')
-    ax[1].set_yscale('log')
-
-    fig.tight_layout()
-    fig.savefig(name)
-
-    data = {}
-    data[r'$RMSE$'] = list(rmses)
-    data[xlabel] = list(moderrs)
-    data['Counts'] = list(counts)
-
-    jsonfile = name+'.json'
-    with open(jsonfile, 'w') as handle:
-        json.dump(data, handle)
-
-
 def make_plots(save):
 
     path = os.path.join(save, 'aggregate')
@@ -103,7 +21,7 @@ def make_plots(save):
         ax.hist(values['ln_likelihood'], bins=20, density=True)
 
         ax.set_ylabel('Counts')
-        ax.set_xlabel('Average Ln Likelihood Metric')
+        ax.set_xlabel('Sum Ln Likelihood Metric')
         fig.tight_layout()
 
         fig.savefig(os.path.join(*[
