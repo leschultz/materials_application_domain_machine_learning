b1607646bbfed877436b31cb5b354335022c36f2
leschultz <laneenriqueschultz@gmail.com>
2021-09-10 14:00:14 -0500

    Expanded the number of suppored datasets.

--- mad/datasets/load_data.py
+++ mad/datasets/load_data.py
@@ -83,6 +83,104 @@ def diffusion():
     return load(df, target, drop_cols)
 
 
+def perovskite_stability():
+    '''
+    Load the perovskite stability dataset.
+    '''
+
+    df = 'Perovskite_stability_Wei_updated_forGlenn.xlsx'
+    target = 'EnergyAboveHull'
+
+    return load(df, target)
+
+
+def electromigration():
+    '''
+    Load the electronmigration dataset.
+    '''
+
+    df = 'Dataset_electromigration.xlsx'
+    target = 'Effective_charge_regression'
+
+    return load(df, target)
+
+
+def thermal_conductivity():
+    '''
+    Load the thermal conductivity dataset.
+    '''
+
+    df = 'citrine_thermal_conductivity_simplified.xlsx'
+    target = 'log(k)'
+
+    return load(df, target)
+
+
+def dielectric_constant():
+    '''
+    Load the dielectric constant dataset.
+    '''
+
+    df = 'dielectric_constant_simplified.xlsx'
+    target = 'log(poly_total)'
+
+    return load(df, target)
+
+
+def double_perovskites_gap():
+    '''
+    Load the double perovskie gap dataset.
+    '''
+
+    df = 'double_perovskites_gap.xlsx'
+    target = 'gap gllbsc'
+
+    return load(df, target)
+
+
+def elastic_tensor():
+    '''
+    Load the elastic tensor dataset.
+    '''
+
+    df = 'elastic_tensor_2015_simplified.xlsx'
+    target = 'log(K_VRH)'
+
+    return load(df, target)
+
+
+def heusler_magnetic():
+    '''
+    Load the heussler magnetic dataset.
+    '''
+
+    df = 'heusler_magnetic_simplified.xlsx'
+    target = 'mu_b saturation'
+
+    return load(df, target)
+
+def piezoelectric_tensor():
+    '''
+    Load the piezoelectric tensor data.
+    '''
+
+    df = 'piezoelectric_tensor.xlsx'
+    target = 'log(eij_max)'
+
+    return load(df, target)
+
+
+def steel_yield_strength():
+    '''
+    Load the steel yield strenght dataset.
+    '''
+
+    df = 'steel_strength_simplified.xlsx'
+    target = 'yield strength'
+
+    return load(df, target)
+
+
 def test():
     '''
     Load the test dataset.
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -6,13 +6,22 @@ class ml_test(unittest.TestCase):
 
     def test_loads(self):
         '''
-        Test data import
+        Test data import for all supported sets.
         '''
 
+        load_data.test()
         load_data.friedman()
         load_data.diffusion()
         load_data.super_cond()
-        load_data.test()
+        load_data.perovskite_stability()
+        load_data.electromigration()
+        load_data.thermal_conductivity()
+        load_data.dielectric_constant()
+        load_data.double_perovskites_gap()
+        load_data.elastic_tensor()
+        load_data.heusler_magnetic()
+        load_data.piezoelectric_tensor()
+        load_data.steel_yield_strength()
 
 
 if __name__ == '__main__':
