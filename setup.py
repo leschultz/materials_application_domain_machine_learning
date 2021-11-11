a962f3975f153746bfcc879c07d19e5e303fffb4
angelotc <angelocortez102@gmail.com>
2021-11-10 21:49:01 -0600

    done

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = setuptools.find_packages()
 install_requires = [
                     'matplotlib',
                     'scipy',
-                    'scikit-learn',
+                    'scikit-learn==0.24.1',
                     'pandas',
                     'numpy',
                     'seaborn',
