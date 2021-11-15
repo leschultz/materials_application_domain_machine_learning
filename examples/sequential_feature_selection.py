58f973d18ebe12b2fa580a9ea82097253635196f
angelotc <angelocortez102@gmail.com>
2021-11-15 14:57:18 -0600

    sequential feature selection

---
+++ examples/sequential_feature_selection.py
@@ -0,0 +1,72 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.linear_model import Lasso
+from sklearn import cluster
+
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from mad.datasets import aggregate, statistics
+from mad.plots import versus
+from mad.plots import parity
+
+from mad.datasets import load_data
+from mad.ml import splitters, predict, feature_selectors
+
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def main():
+    '''
+    Test ml workflow
+    '''
+
+    seed = 14987
+    save = 'run'
+    points = None
+    sampling = None
+
+    # Load data
+    data = load_data.diffusion()
+    df = data['frame']
+    X = data['data']
+    y = data['target']
+
+    grouping = data['class_name']
+
+    # ML setup
+    scale = feature_selectors.sequential_vif_selection(n_features=22)
+    outer_split = KFold(n_splits=5)
+    inner_split = KFold(n_splits=5)
+    selector = feature_selectors.no_selection()
+
+   
+
+    # Do LASSO
+    model = BaggingRegressor(base_estimator=Lasso())
+    grid = {}
+    grid['model__base_estimator__alpha'] = np.logspace(-5, 5, 11)
+    pipe = Pipeline(steps=[
+                           ('scaler', scale),
+                           ('select', selector),
+                           ('model', model)
+                           ])
+    lasso = GridSearchCV(pipe, grid, cv=inner_split)
+
+    # Make pipeline
+    pipes = [lasso]
+
+    # Evaluates
+    predict.run(X, y, outer_split, pipes, save, seed, groups=grouping)
+    aggregate.folds(save)  # Combine split data from directory recursively
+    statistics.folds(save)  # Gather statistics from data
+    parity.make_plots(save)  # Make parity plots
+    versus.make_plots(save, points, sampling)
+
+
+if __name__ == '__main__':
+    main()
--- mad/ml/feature_selectors.py
+++ mad/ml/feature_selectors.py
@@ -1,3 +1,4 @@
+from sklearn import feature_selection
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 import pandas as pd
 
@@ -23,13 +24,13 @@ class vif_selection:
     A class for VIF score for feature selection.
     '''
 
-    def __init__(self, percent=0.8):
+    def __init__(self, n_features):
         '''
         inputs:
-            percent = gives the top percent of feature.
+            n_features = number of features to choose
         '''
 
-        self.percent = percent
+        self.n_features = n_features
 
     def transform(self, X):
         '''
@@ -46,12 +47,59 @@ class vif_selection:
 
         vif['features_index'] = feature_indices
         vif_sorted = vif.sort_values('VIF Factor')
-
-        num_to_select = int(len(feature_indices)*self.percent)
-        feature_selected = list(vif_sorted['features_index'])[:num_to_select]
+        print(vif_sorted)
+        feature_selected = list(vif_sorted['features_index'])[:self.n_features]
 
         X = X[:, feature_selected]
 
+        return X
+
+    def fit(self, X, y=None):
+        return self
+
+class sequential_vif_selection:
+    '''
+    A class for VIF score for feature selection.
+    '''
+
+    def __init__(self, n_features):
+        '''
+        inputs:
+            percent = gives the top percent of feature.
+        '''
+
+        self.n_features = n_features
+
+    def transform(self, X):
+        '''
+        Find the top n_features of features, and transform the data.
+        '''
+
+        #Instantiate DF , first iteration
+        
+        vif_df = pd.DataFrame()
+        feature_indices = list(range(X.shape[1]))
+        vals = [variance_inflation_factor(X, i) for i in feature_indices]
+        vif_df['VIF Factor'] = vals
+        vif_df['features_index'] = feature_indices
+        vif_sorted = vif_df.sort_values('VIF Factor')
+        #remove 1 feature until there are self.n_features left
+        while len(vals)> self.n_features:
+            # remove 1 feature
+            features_selected = list(vif_sorted['features_index'])[:len(vals)-1]
+            X = X[:, features_selected]
+            if features_selected == self.n_features:
+                break
+
+            # Preparation for next iteration; re-calculate VIF of every variable 
+            feature_indices = list(range(X.shape[1]))
+            vals = [variance_inflation_factor(X, i) for i in feature_indices]
+            
+            vif_df = pd.DataFrame()
+            vif_df['VIF Factor'] = vals
+            vif_df['features_index'] = feature_indices
+            vif_sorted = vif_df.sort_values('VIF Factor')
+        print("SENDING X WITH {} FEATURES".format(str(len([variance_inflation_factor(X, i) for i in feature_indices]))))
         return X
 
     def fit(self, X, y=None):
