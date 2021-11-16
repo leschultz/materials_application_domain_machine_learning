2071383ec66c7b0bf731725b301eded2f18c7c44
angelotc <angelocortez102@gmail.com>
2021-11-15 22:04:07 -0600

    this

--- mad/ml/feature_selectors.py
+++ mad/ml/feature_selectors.py
@@ -22,20 +22,17 @@ class vif_selection:
     '''
     A class for VIF score for feature selection.
     '''
-
     def __init__(self, n_features):
         '''
         inputs:
             n_features = number of features to choose
         '''
-
         self.n_features = n_features
 
     def transform(self, X):
         '''
         Find the top percent of features, and transform the data.
         '''
-
         feature_indices = list(range(X.shape[1]))
         assert(self.n_features <= len(feature_indices)), "n_features ({}) needs to be less than or equal to the amount of features in the dataset ({})".format(
             str(self.n_features), str(len(feature_indices)))
@@ -44,7 +41,6 @@ class vif_selection:
         for i in feature_indices:
             vals.append(variance_inflation_factor(X, i))
         vif['VIF Factor'] = vals
-
         vif['features_index'] = feature_indices
         vif_sorted = vif.sort_values('VIF Factor')
         feature_selected = list(vif_sorted['features_index'])[:self.n_features]
@@ -60,22 +56,18 @@ class sequential_vif_selection:
     '''
     A class for VIF score for feature selection. Works with all features, then removes them one by one.
     '''
-
     def __init__(self, n_features):
         '''
         inputs:
             percent = gives the top percent of feature.
         '''
-        
         self.n_features = n_features
 
     def transform(self, X):
         '''
         Find the top n_features of features, and transform the data.
         '''
-
         #Instantiate DF , first iteration
-        
         vif_df = pd.DataFrame()
         feature_indices = list(range(X.shape[1]))
         assert(self.n_features <= len(feature_indices)), "n_features ({}) needs to be less than or equal to the amount of features in the dataset ({})".format(
@@ -91,11 +83,9 @@ class sequential_vif_selection:
             X = X[:, features_selected]
             if features_selected == self.n_features:
                 break
-
             # Preparation for next iteration; re-calculate VIF of every variable 
             feature_indices = list(range(X.shape[1]))
             vals = [variance_inflation_factor(X, i) for i in feature_indices]
-            
             vif_df = pd.DataFrame()
             vif_df['VIF Factor'] = vals
             vif_df['features_index'] = feature_indices
