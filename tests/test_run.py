1ea28afb9cf899e0a2466acec60e4a89deba776c
leschultz <laneenriqueschultz@gmail.com>
2022-02-10 11:05:13 -0600

    Fixed stest run

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 from mad.datasets import load_data, statistics
-from mad.plots import parity, calibration, err_in_err, nllh_versus
+from mad.plots import parity, calibration
 from mad.ml import splitters, domain, feature_selectors
 from mad.functions import poly
 
@@ -73,8 +73,6 @@ class ml_test(unittest.TestCase):
         statistics.folds(save)  # Gather statistics from data
         parity.make_plots(save, 'pdf')  # Make parity plots
         calibration.make_plots(save, 10, 'stdcal', 'pdf')
-        err_in_err.make_plots(save, 10, 'mahalanobis')
-        nllh_versus.make_plots(save, 10, 'mahalanobis')
 
         # Clean directory
         shutil.rmtree(save)
