90202ca95d85756ff9c71584cf3325615b98664f
leschultz <laneenriqueschultz@gmail.com>
2022-02-16 09:45:04 -0600

    Now added calibration values to precission recall curve.

--- mad/plots/pr.py
+++ mad/plots/pr.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import precision_recall_curve, auc
+from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as pl
 
 import pandas as pd
@@ -12,46 +13,56 @@ def make_plot(save, score):
     '''
 
     df = pd.read_csv(os.path.join(save, 'aggregate/data.csv'))
-    df = df[['in_domain', score]]
+    df = df[['in_domain', score, 'stdcal']]
     df = df.loc[df['in_domain'] != 'td']
     df = df[df[score].notna()]
 
     y_true = [0 if i == 'id' else 1 for i in df['in_domain'].values]
-    y_scores = df[score].values
+    scaler = StandardScaler()
 
-    precision, recall, thresholds = precision_recall_curve(y_true, y_scores)
-    baseline = sum(y_true)/len(y_true)
-    auc_score = auc(recall, precision)
+    for cols in [[score], [score, 'stdcal']]:
+        y_scores = df[cols].values
 
-    f1_scores = 2*recall*precision/(recall+precision)
-    max_f1 = np.max(f1_scores)
+        scaler.fit(y_scores)
+        y_scores = scaler.transform(y_scores)
+        y_scores = np.mean(y_scores, axis=1)
 
-    fig, ax = pl.subplots()
+        precision, recall, thresholds = precision_recall_curve(
+                                                               y_true,
+                                                               y_scores
+                                                               )
+        baseline = sum(y_true)/len(y_true)
+        auc_score = auc(recall, precision)
 
-    ax.plot(
-            recall,
-            precision,
-            color='b',
-            label='AUC={:.2f}\nMax F1={:.2f}'.format(auc_score, max_f1)
-            )
-    ax.axhline(baseline, color='r', linestyle=':', label='Baseline')
-    ax.set_xlim(0.0, 1.05)
-    ax.set_ylim(0.0, 1.05)
-    ax.set_xlabel('Recall')
-    ax.set_ylabel('Precision')
-    ax.legend()
+        f1_scores = 2*recall*precision/(recall+precision)
+        max_f1 = np.max(f1_scores)
 
-    fig.tight_layout()
-    name = [
-            save,
-            'aggregate',
-            'plots',
-            'total',
-            'precision_recall',
-            score
-            ]
-    name = map(str, name)
-    name = os.path.join(*name)
-    os.makedirs(name, exist_ok=True)
-    name = os.path.join(name, 'precision_recall.png')
-    fig.savefig(name)
+        fig, ax = pl.subplots()
+
+        ax.plot(
+                recall,
+                precision,
+                color='b',
+                label='AUC={:.2f}\nMax F1={:.2f}'.format(auc_score, max_f1)
+                )
+        ax.axhline(baseline, color='r', linestyle=':', label='Baseline')
+        ax.set_xlim(0.0, 1.05)
+        ax.set_ylim(0.0, 1.05)
+        ax.set_xlabel('Recall')
+        ax.set_ylabel('Precision')
+        ax.legend()
+
+        fig.tight_layout()
+        name = [
+                save,
+                'aggregate',
+                'plots',
+                'total',
+                'precision_recall',
+                '_'.join(cols)
+                ]
+        name = map(str, name)
+        name = os.path.join(*name)
+        os.makedirs(name, exist_ok=True)
+        name = os.path.join(name, 'precision_recall.png')
+        fig.savefig(name)
