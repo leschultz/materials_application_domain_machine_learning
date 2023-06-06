aadce879b50c326866ebe149c23916450ee64148
leschultz <laneenriqueschultz@gmail.com>
2023-06-06 12:22:19 -0500

    Fixed outomated testing

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,4 +1,4 @@
-from madml.datasets import load_data
+from madml import datasets
 import unittest
 
 
@@ -9,19 +9,9 @@ class ml_test(unittest.TestCase):
         Test data import for all supported sets.
         '''
 
-        load_data.friedman1()
-        load_data.fluence()
-        load_data.diffusion()
-        load_data.super_cond()
-        load_data.perovskite_stability()
-        load_data.electromigration()
-        load_data.thermal_conductivity()
-        load_data.dielectric_constant()
-        load_data.double_perovskites_gap()
-        load_data.elastic_tensor()
-        load_data.heusler_magnetic()
-        load_data.piezoelectric_tensor()
-        load_data.steel_yield_strength()
+        for i in datasets.list_data():
+            print(i)
+            datasets.load(i)
 
 
 if __name__ == '__main__':
