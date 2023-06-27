c796d6c72fbf66b8221c7529109827a9ed6c16b6
leschultz <laneenriqueschultz@gmail.com>
2023-06-27 09:55:12 -0500

    Incrmented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 # Package information
 name = 'madml'
-version = '0.1.3'
+version = '0.1.4'
 description = 'Application domain of machine learning in materials science.'
 url = 'https://github.com/leschultz/'\
       'materials_application_domain_machine_learning.git'
