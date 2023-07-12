4786a229a73234528fad0405f441ab16a049cf41
leschultz <laneenriqueschultz@gmail.com>
2023-07-12 14:47:31 -0500

    Changed the default kernel for kde space model

--- src/madml/models/space.py
+++ src/madml/models/space.py
@@ -39,9 +39,14 @@ class distance_model:
             bw = estimate_bandwidth(X_train)
 
             if bw > 0.0:
-                self.model = KernelDensity(bandwidth=bw).fit(X_train)
+                self.model = KernelDensity(
+                                           kernel='tophat',
+                                           bandwidth=bw,
+                                           ).fit(X_train)
             else:
-                self.model = KernelDensity().fit(X_train)
+                self.model = KernelDensity(
+                                           kernel='tophat',
+                                           ).fit(X_train)
 
             dist = self.model.score_samples(X_train)
             self.scaler = lambda x: 1-np.exp(x-max(dist))
