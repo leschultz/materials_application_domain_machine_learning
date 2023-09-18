a2b14990d84f9ae18548cceac9f0bf52c333cc92
leschultz <laneenriqueschultz@gmail.com>
2023-09-18 11:34:54 -0500

    Remove the univariate KDE because it does worse in the statistical tests

--- src/madml/models/space.py
+++ src/madml/models/space.py
@@ -5,85 +5,6 @@ from scipy.spatial.distance import cdist
 import numpy as np
 
 
-class weighted_kde:
-
-    def __init__(
-                 self,
-                 kernel,
-                 bandwidth=None,
-                 weigh=False,
-                 weights=None,
-                 ):
-
-        self.bandwidth = bandwidth
-        self.weights = weights
-        self.weigh = weigh
-        self.kernel = kernel
-
-    def fit(self, X_train):
-        '''
-        Build kernel density estimate models for each feature.
-
-        inputs:
-            X_train: The training space of the model.
-        '''
-
-        self.counts = range(X_train.shape[1])
-        self.models = []
-        self.bandwidths = []
-
-        for b in self.counts:
-
-            cut = b+1
-
-            if self.bandwidth is None:
-                bandwidth = estimate_bandwidth(X_train[:, b:cut])
-            else:
-                bandwidth = self.bandwidth
-
-            if self.weigh == 'bandwidths':
-                bandwidth = self.weights[b]*bandwidth
-
-            if bandwidth > 0.0:
-
-                model = KernelDensity(
-                                      kernel=self.kernel,
-                                      bandwidth=bandwidth,
-                                      ).fit(X_train[:, b:cut])
-            else:
-                model = None
-
-            self.models.append(model)
-            self.bandwidths.append(bandwidth)
-
-    def score_samples(self, X):
-        '''
-        A method to use each feature KDE model to predict on new data.
-
-        inputs:
-            X = The features and cases.
-        '''
-
-        scores = []
-        for b in self.counts:
-
-            if self.models[b] is None:
-                continue
-
-            score = self.models[b].score_samples(X[:, b:b+1])
-
-            if self.weigh == 'scores':
-                indx = score != -np.inf
-                score[indx] = score[indx]*self.weights[b]
-
-            scores.append(score)
-
-        return np.sum(scores, axis=0)
-
-    def return_bandwidths(self):
-        return self.bandwidths
-
-
 class distance_model:
 
     def __init__(
@@ -129,16 +50,14 @@ class distance_model:
             if 'bandwidth' in self.kwargs.keys():
                 self.bandwidth = self.kwargs['bandwidth']
             else:
-                self.bandwidth = None
+                self.bandwidth = estimate_bandwidth(X_train)
 
-            self.model = weighted_kde(
-                                      self.kernel,
-                                      weigh=self.weigh,
-                                      weights=self.weights,
-                                      bandwidth=self.bandwidth,
-                                      )
+            self.model = KernelDensity(
+                                       kernel=self.kernel,
+                                       bandwidth=self.bandwidth,
+                                       )
+
             self.model.fit(X_train)
-            self.bandwidth = self.model.bandwidths
 
             dist = self.model.score_samples(X_train)
             m = np.max(dist)
