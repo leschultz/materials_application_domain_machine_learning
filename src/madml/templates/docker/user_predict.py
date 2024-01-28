6b281c0d536c0fbd27ca761114d9dc483b988004
leschultz <laneenriqueschultz@gmail.com>
2024-01-28 16:35:31 -0600

    Updated some of the templates

--- src/madml/templates/docker/user_predict.py
+++ src/madml/templates/docker/user_predict.py
@@ -1,9 +1,10 @@
-from madml.hosting.docker import predict
+from madml.hosting.docker import dockerhub_model
 import numpy as np
 
 container_name = 'replace'
+model = dockerhub_model(container_name)
 
 X = np.loadtxt('X.csv', delimiter=',')
+y = model.predict(X)
 
-y = predict(X, container_name)
 print(y)
